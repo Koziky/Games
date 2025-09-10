4b07187070c958cee66af846df0330b1e9f99e62
Koziky <Koz1ky22@gmail.com>
2025-09-10 18:47:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Simple demo script for progress bar
-document.addEventListener("DOMContentLoaded", () => {
-  const progress = document.querySelector(".braflix-progress-indicator");
+// Sample movie cards
+const movies = [
+  { title: "Inception", img: "https://i.imgur.com/Yo5jJ8k.jpeg" },
+  { title: "Interstellar", img: "https://i.imgur.com/4n8xqdY.jpeg" },
+  { title: "Gravity", img: "https://i.imgur.com/4GVtC2K.jpeg" },
+  { title: "Ad Astra", img: "https://i.imgur.com/Fx0qG3s.jpeg" },
+  { title: "The Martian", img: "https://i.imgur.com/hpRoFHF.jpeg" },
+];
 
-  let percent = 40;
-  setInterval(() => {
-    percent = (percent + 10) % 110;
-    progress.style.width = percent + "%";
-  }, 2000);
+const row = document.getElementById("movie-row");
+
+movies.forEach((movie) => {
+  const card = document.createElement("div");
+  card.className = "movie-card";
+  card.innerHTML = `<img src="${movie.img}" alt="${movie.title}" />`;
+  row.appendChild(card);
 });
